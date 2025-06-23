7c8bd1b19a0a320a69146e0a625eae55fb968654
Miroslav Kozma <xkozma@stuba.sk>
2025-06-23 16:40:11 +0200

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -2,6 +2,6 @@
 
 # Navigate to the directory containing the Electron app
 cd "$(dirname "$0")"
-python ./python/assistant.py
+python ./python/assistant.py &
 # Start the Electron app using npm
 npm start
