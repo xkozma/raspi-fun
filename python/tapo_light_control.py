db6fca9834843991809ac68a9961ae281cc71e1f
Miroslav Kozma <xkozma@stuba.sk>
2025-06-25 15:38:18 +0200

    Update tapo_light_control.py

--- python/tapo_light_control.py
+++ python/tapo_light_control.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv()
 async def main():
     client = ApiClient(os.getenv("TAPO_NAME"),os.getenv("TAPO_PASSWORD"))
     device = await client.l900(os.getenv("TAPO_IP"))
-    await device.off()
+    await device.on()
     info = await device.get_device_info_json()
     print(f"Device Info: {info}")
 
